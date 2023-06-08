a5ebb8bf5712128f071a8008b61add806062cf83
wuqingze <nongxiaolang@foxmail.com>
2023-06-08 11:36:16 +0800

    fix bug:Uncaught TypeError: Cannot read properties of null (reading 'addEventListener')

--- content-script.js
+++ content-script.js
@@ -164,7 +164,7 @@ function cleanpage(){
 
 function run() {
     var e = document.querySelector("#__next > div > div > aside > div > header > a");
-    e.addEventListener("click", function(event) {
+    if(e) e.addEventListener("click", function(event) {
         event.preventDefault(); // 阻止默认行为，即打开链接
         // 这里添加您想要执行的代码
         copy();
